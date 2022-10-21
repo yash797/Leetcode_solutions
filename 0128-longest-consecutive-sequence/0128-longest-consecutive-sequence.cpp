d106064752571700fa108e34ab4b557eedce5ec2
yash797 <80116786+yash797@users.noreply.github.com>
2022-10-22 01:10:53 +0530

    Update 0128-longest-consecutive-sequence.cpp

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -10,5 +10,33 @@ public:
 		else longest = max(longest, cur_longest), cur_longest = 1;  // reset current streak length
 	return max(longest, cur_longest);
 }
+	
+	
+	
+// 	ye wala kyon nhi chal raha bhagwan jaane (tle aaraha)
+// 	purra same hai upr jaisa
+	int longestConsecutive(vector<int>& nums) 
+    {
+        if(!size(nums)) 
+        return 0;
+        sort(begin(nums),end(nums));
+        int maxi=0;
+        int count=1;
+        for(int i=0; i<size(nums);i++){
+            if(nums[i]==nums[i-1]){
+                continue;
+            }
+        else if(nums[i]=nums[i-1]+1)
+            count++;
+        else
+            maxi=max(count,maxi);
+            count=1;
+        }
+    
+    
+    return max(maxi,count);
+        
+        
+    }
 
 };
